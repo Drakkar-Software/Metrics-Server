aa4c26f75d7a2b17e2f6067866b7b642c9d61965
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-04-08 23:56:39 +0200

    updated community route

--- api/route/telemetryroute.go
+++ api/route/telemetryroute.go
@@ -7,7 +7,7 @@ import (
 
 // Init registers the server routes
 func Init(e *echo.Echo) {
-	e.GET("/", controller.PublicGetBots)
+	e.GET("/community", controller.PublicGetBots)
 	e.GET("/gen-bot-id", controller.GenerateBotID)
 	e.POST("/metrics/uptime", controller.UpdateBotUptime)
 	e.POST("/metrics/register", controller.RegisterBot)
