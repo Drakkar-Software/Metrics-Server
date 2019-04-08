21c37e5cb5752ec11ae6abc8022e771e0ad7cc41
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-04-09 00:03:20 +0200

    fixed metrics route

--- api/route/telemetryroute.go
+++ api/route/telemetryroute.go
@@ -7,8 +7,8 @@ import (
 
 // Init registers the server routes
 func Init(e *echo.Echo) {
-	e.GET("/community", controller.PublicGetBots)
 	e.GET("/gen-bot-id", controller.GenerateBotID)
+	e.GET("/metrics/community", controller.PublicGetBots)
 	e.POST("/metrics/uptime", controller.UpdateBotUptime)
 	e.POST("/metrics/register", controller.RegisterBot)
 }
