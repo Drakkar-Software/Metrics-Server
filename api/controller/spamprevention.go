4f4a8f8f5be67ccb5e536b6517625a44119215f0
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-04-13 21:43:10 +0200

    added rate limits

---
+++ api/controller/spamprevention.go
@@ -0,0 +1,69 @@
+package controller
+
+import (
+	"log"
+	"sync"
+	"time"
+
+	"github.com/labstack/echo"
+)
+
+var maxRequestsPerDay = uint8(100)
+
+type requestStats struct {
+	Day          int
+	RequestCount uint8
+	mutex        sync.RWMutex
+}
+
+func newRequestStats() *requestStats {
+	return &requestStats{
+		Day:          time.Now().Day(),
+		RequestCount: 1,
+	}
+}
+
+func (req *requestStats) newDay(day int) {
+	req.mutex.Lock()
+	defer req.mutex.Unlock()
+	req.Day = day
+	req.RequestCount = 1
+}
+
+func (req *requestStats) isMaxedForToday() bool {
+	return req.RequestCount >= maxRequestsPerDay
+}
+
+func (req *requestStats) incCounter() {
+	req.mutex.Lock()
+	defer req.mutex.Unlock()
+	req.RequestCount++
+}
+
+var requestStatsByIP = make(map[string]*requestStats)
+
+// IsIPAllowed returns false if API is getting spammed
+func IsIPAllowed(c echo.Context) bool {
+	ip := c.RealIP()
+	log.Println(ip)
+	stats, exists := requestStatsByIP[ip]
+	if exists {
+		// not first request
+		now := time.Now().Day()
+		if stats.Day != now {
+			// new day: reset counter
+			stats.newDay(now)
+		} else {
+			// check counter
+			if stats.isMaxedForToday() {
+				log.Println("Spam attack")
+				return false
+			}
+			stats.incCounter()
+		}
+	} else {
+		// first request: start stats
+		requestStatsByIP[ip] = newRequestStats()
+	}
+	return true
+}
--- api/controller/telemetrycontroller.go
+++ api/controller/telemetrycontroller.go
@@ -11,7 +11,7 @@ import (
 	"github.com/labstack/echo"
 )
 
-// GetBots returns a json representation of all the bots
+// PublicGetBots returns a json representation of all the bots
 func PublicGetBots(c echo.Context) error {
 	bots, err := dao.PublicGetBots()
 	if err != nil {
@@ -24,43 +24,49 @@ func PublicGetBots(c echo.Context) error {
 
 // GenerateBotID returns a new bot ID
 func GenerateBotID(c echo.Context) error {
-	id, err := dao.GenerateBotID()
-	if err != nil {
-		log.Println(err)
-		return c.JSON(http.StatusBadRequest, id)
+	if IsIPAllowed(c) {
+		id, err := dao.GenerateBotID()
+		if err != nil {
+			log.Println(err)
+			return c.JSON(http.StatusBadRequest, id)
+		}
+		return c.JSON(http.StatusOK, id)
 	}
-	return c.JSON(http.StatusOK, id)
+	return c.JSON(http.StatusTooManyRequests, nil)
 }
 
 // UpdateBotUptime updates a bot uptime
 func UpdateBotUptime(c echo.Context) error {
-	bot := new(model.Bot)
-	c.Bind(bot)
-	id, err := dao.UpdateBotUptime(bot)
-	if err != nil {
-		if err == dao.ErrBotNotFound {
-			return c.JSON(http.StatusNotFound, id)
-		} else {
+	if IsIPAllowed(c) {
+		bot := new(model.Bot)
+		c.Bind(bot)
+		id, err := dao.UpdateBotUptime(bot)
+		if err != nil {
+			if err == dao.ErrBotNotFound {
+				return c.JSON(http.StatusNotFound, id)
+			}
 			log.Println(err, bot.ID)
 			return c.JSON(http.StatusBadRequest, id)
 		}
+		return c.JSON(http.StatusOK, id)
 	}
-
-	return c.JSON(http.StatusOK, id)
+	return c.JSON(http.StatusTooManyRequests, nil)
 }
 
 // RegisterBot registers a bot as started (creates a new bot if necessary)
 func RegisterBot(c echo.Context) error {
-	bot := new(model.Bot)
-	c.Bind(bot)
-	id, err := dao.RegisterOrUpdate(bot)
-	if err != nil {
-		if err == dao.ErrBotNotFound {
-			return c.JSON(http.StatusNotFound, id)
-		} else {
+	if IsIPAllowed(c) {
+		bot := new(model.Bot)
+		c.Bind(bot)
+		id, err := dao.RegisterOrUpdate(bot)
+		if err != nil {
+			if err == dao.ErrBotNotFound {
+				return c.JSON(http.StatusNotFound, id)
+			}
 			log.Println(err, bot.ID)
 			return c.JSON(http.StatusBadRequest, id)
 		}
+		return c.JSON(http.StatusOK, id)
 	}
-	return c.JSON(http.StatusOK, id)
+	return c.JSON(http.StatusTooManyRequests, nil)
 }
