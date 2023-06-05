dd41c69306a18e69c61fad7fdb5b8cad3b93f656
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2023-06-05 23:25:05 +0200

    [Cache] fix identifier collision

--- api/caches/botcountcache.go
+++ api/caches/botcountcache.go
@@ -2,23 +2,37 @@ package caches
 
 // BotCountsCache stores bot counts cache values
 type BotCountsCache struct {
-	Counts         map[int64]int64
+	Counts       map[int64]int64
+	UpdatesTimes map[int64]int64
 }
 
 var localBotCountsCache = BotCountsCache{
-	Counts: make(map[int64]int64),
+	Counts:       make(map[int64]int64),
+	UpdatesTimes: make(map[int64]int64),
 }
 
 // GetBotCount returns the bot count from asked cache value and a boolean for value availability
-func GetBotCount(identifier int64) (int64, bool) {
-	value, exists := localBotCountsCache.Counts[roundIdentifier(identifier)]
-	return value, exists
+func GetUpToDateBotCount(identifier int64, nowTime int64) (int64, bool) {
+	key := roundIdentifier(identifier)
+	value, exists := localBotCountsCache.Counts[key]
+	if !exists {
+		return value, false
+	}
+	updateTime, _ := localBotCountsCache.UpdatesTimes[key]
+	// value is outdated
+	if nowTime-updateTime > CacheRefreshPeriod/2 {
+		return value, false
+	}
+	return value, true
 }
 
 // SetBotCount sets the cache value for a given time. Resets cache when too many values in it
-func SetBotCount(identifier int64, count int64) {
+func SetBotCount(identifier int64, count int64, updateTime int64) {
 	if len(localBotCountsCache.Counts) > maxCacheSize {
 		localBotCountsCache.Counts = make(map[int64]int64)
+		localBotCountsCache.UpdatesTimes = make(map[int64]int64)
 	}
-	localBotCountsCache.Counts[roundIdentifier(identifier)] = count
+	key := roundIdentifier(identifier)
+	localBotCountsCache.Counts[key] = count
+	localBotCountsCache.UpdatesTimes[key] = updateTime
 }
